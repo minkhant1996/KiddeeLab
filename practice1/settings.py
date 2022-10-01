be90b352b5c266e4e049e44a02c0fb403f2f60be
minkhant1996 <69729266+minkhant1996@users.noreply.github.com>
2022-10-01 15:52:47 +0700

    change setting

--- practice1/settings.py
+++ practice1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dpbg98z2scc9jef3(!k(6sh80vkw)1o$*o4t88l-*k$^-v11v5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','kiddeelabthailand.herokuapp.com']
 
 
 # Application definition
