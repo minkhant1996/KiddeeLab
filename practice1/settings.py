d7a27b976263d4bb95f91014a65e23bed57ee472
minkhant1996 <69729266+minkhant1996@users.noreply.github.com>
2022-10-01 16:29:28 +0700

    new setting

--- practice1/settings.py
+++ practice1/settings.py
@@ -130,7 +130,7 @@ MEDIA_URL = '/media/'
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
