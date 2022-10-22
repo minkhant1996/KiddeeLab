af9396d48e3e038f50bf73204717cd2a61c18e63
minkhant1996 <69729266+minkhant1996@users.noreply.github.com>
2022-10-22 15:12:23 +0700

    updated setting  commit

--- practice1/settings.py
+++ practice1/settings.py
@@ -11,21 +11,36 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
+from django.test.runner import DiscoverRunner
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+IS_HEROKU = "DYNO" in os.environ
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-dpbg98z2scc9jef3(!k(6sh80vkw)1o$*o4t88l-*k$^-v11v5'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+if 'SECRET_KEY' in os.environ:
+    SECRET_KEY = os.environ["SECRET_KEY"]
 
-ALLOWED_HOSTS = ['127.0.0.1','kiddeelabthailand.herokuapp.com']
+
+# Generally avoid wildcards(*). However since Heroku router provides hostname validation it is ok
+if IS_HEROKU:
+    # ALLOWED_HOSTS = ["*"]
+    ALLOWED_HOSTS = ['127.0.0.1','kiddeelabthailand.herokuapp.com']
+else:
+    # ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ['127.0.0.1']
+# SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = False
+if not IS_HEROKU:
+    DEBUG = True
+
+# ALLOWED_HOSTS = ['127.0.0.1','kiddeelabthailand.herokuapp.com']
 
 
 # Application definition
@@ -75,6 +90,7 @@ WSGI_APPLICATION = 'practice1.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+MAX_CONN_AGE = 600
 
 DATABASES = {
     'default': {
@@ -82,6 +98,22 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+#     }
+# }
+
+if "DATABASE_URL" in os.environ:
+    # Configure Django for DATABASE_URL environment variable.
+    DATABASES["default"] = dj_database_url.config(
+        conn_max_age=MAX_CONN_AGE, ssl_require=True)
+
+    # Enable test database if found in CI environment.
+    if "CI" in os.environ:
+        DATABASES["default"]["TEST"] = DATABASES["default"]
 
 
 # Password validation
@@ -134,3 +166,28 @@ db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+# Test Runner Config
+class HerokuDiscoverRunner(DiscoverRunner):
+    """Test Runner for Heroku CI, which provides a database for you.
+    This requires you to set the TEST database (done for you by settings().)"""
+
+    def setup_databases(self, **kwargs):
+        self.keepdb = True
+        return super(HerokuDiscoverRunner, self).setup_databases(**kwargs)
+
+
+# Use HerokuDiscoverRunner on Heroku CI
+if "CI" in os.environ:
+    TEST_RUNNER = "gettingstarted.settings.HerokuDiscoverRunner"
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+# GS_BUCKET_NAME = 'YOUR_BUCKET_NAME_GOES_HERE'
+# STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+
+# from django.core.files.storage import default_storage
+# print(default_storage.__class__)
